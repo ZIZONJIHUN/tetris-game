813f69aa0fb363ae46dcc75c8a500bf793a6fa65
jihun <happyhunji@gmail.com>
2026-04-11 13:46:43 +0900

    chore: fix jest config testMatch, remove ts-jest, fix lint script

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = createJestConfig({
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
-  testMatch: ['<rootDir>/src/__tests__/**/*.{ts,tsx}', '<rootDir>/src/__tests__/**/*.test.{ts,tsx}'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}', '<rootDir>/src/**/*.spec.{ts,tsx}'],
 })
